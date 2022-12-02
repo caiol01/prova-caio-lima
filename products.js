3f69ae95526685289a003530f28fcce3c478f3e7
caiolimaa.2001@gmail.com <caiolimaa.2001@gmail.com>
2022-12-01 21:08:31 -0300

    Listar produtos por valor

--- products.js
+++ products.js
@@ -1,32 +1,44 @@
 var produtos = [
     {
         nomeProduto: 'Siena',
-        categoria: 'carro'
+        categoria: 'carro',
+        valor: "20000"
     },
     {
         nomeProduto: 'Hornet',
-        categoria: 'moto'
+        categoria: 'moto',
+        valor: "44000"
     },
     {
         nomeProduto: 'daily 35s14',
-        categoria: 'caminhao'
+        categoria: 'caminhao',
+        valor: "140000"
     },
     {
         nomeProduto: 'C3',
-        categoria: 'carro'
+        categoria: 'carro',
+        valor: "68000"
     },
     {
         nomeProduto: 'Kawasaki',
-        categoria: 'moto'
+        categoria: 'moto',
+        valor: "37000"
     },
     {
         nomeProduto: 'tector',
-        categoria: 'caminhao'
+        categoria: 'caminhao',
+        valor: "350000"
     }
 ]
 
 function listarPorCategoria(categoria) {
     let filtrarPorProduto = []
     filtrarPorProduto = produtos.filter(produto => (produto.categoria === categoria));
+    return filtrarPorProduto;
+}
+
+function listarPorValor(valorMinimo, valorMaximo) {
+    let filtrarPorProduto = [];
+    filtrarPorProduto = produtos.filter(produto => (produto.valorMinimo >= valorMinimo && produto.valorMaximo <= valorMaximo));
     return filtrarPorProduto;
 }
